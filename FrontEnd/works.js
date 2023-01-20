99a6aabb46c3ab85936fbedfb59fc69d2c3434df
Immortalisanto <tur.anthony91@gmail.com>
2023-01-20 13:28:20 +0100

    Préparation du projet-JS

---
+++ FrontEnd/works.js
@@ -0,0 +1,5 @@
+// Récupération des projets
+const works = await fetch("http://localhost:5678/api/works").then(works => works.json());
+
+
+// Bientôt une boucle ici pour englober l'affichage d'un projet
